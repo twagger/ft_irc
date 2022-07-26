9372c4d45bf301cb21e3c47b97529a81891ca260
twagner <twagner@student.42.fr>
2022-07-26 16:18:06 +0200

    feat: user mod management with bitwise

--- includes/user.hpp
+++ includes/user.hpp
@@ -12,14 +12,23 @@
 # define ST_PINGED 2
 
 //   The available modes are as follows:
-//		none								=> 0	000
-//		a - user is flagged as away;		=> 1	001			
-//     	i - marks a users as invisible; 	=> 2	010
-//      w - user receives wallops; 			=> 3	011
-//      r - restricted user connection; 	=> 4	100
-//      o - operator flag; 					=> 5	101 
-//      O - local operator flag; 			=> 6	110	
-//      s - marks a user for receipt of server notices. => 7	111
+//		none ?												=> 0	0000 0000
+//		a - user is flagged as away;						=> 1	0000 0001
+//		w - user receives wallops;							=> 2	0000 0010			
+//     	i - marks a users as invisible; 					=> 3	0000 0100
+//      o - operator flag;  								=> 4	0000 1000
+//      O - local operator flag;							=> 5	0001 0000
+//      s - marks a user for receipt of server notices.		=> 6	0010 0000
+//      r - restricted user connection;	 					=> 7	0100 0000
+
+#define MOD_NONE        (0 << 0)
+#define MOD_AWAY        (1 << 0)
+#define MOD_WALLOPS     (1 << 2)
+#define MOD_INVISIBLE   (1 << 3)
+#define MOD_OPER        (1 << 4)
+#define MOD_LOCALOP     (1 << 5)
+#define MOD_SRVNOTICES  (1 << 6)
+#define MOD_RESTRICTED  (1 << 7)
 
 class User {
 
@@ -29,7 +38,7 @@ class User {
 		std::string					_username;			
 		std::string 				_fullname;
 		std::string					_hostname;
-		uint16_t					_mode;
+		uint8_t					    _mode;
 		bool						_password;
 		bool						_authenticated;
 		std::vector<std::string>	_channelsJoined;
@@ -50,7 +59,7 @@ class User {
 		std::string 				getUsername(void) const;
 		std::string 				getFullname(void) const;
 		std::string 				getHostname(void) const;
-		uint16_t 					getMode(void) const;
+		uint8_t 					getMode(void) const;
 		bool 						getPassword(void) const;
 		bool 						getAuthenticated(void) const;
 		std::vector<std::string>	getChannelsJoined(void) const;
@@ -63,13 +72,17 @@ class User {
 		void setUsername(std::string username);
 		void setFullname(std::string fullname);
 		void setHostname(std::string fullname);
-		void setMode(uint16_t mode);
 		void setPassword(bool pass);
 		void setAuthenticated(bool authenticated);
 		void setStatus(int status);
 		void setLastActivityTime(void);
 		void setPingTime(void);
-		
+
+        // mode
+		void addMode(uint8_t mode);
+		void removeMode(uint8_t mode);
+        bool hasMode(uint8_t mode);
+
 		bool addChannelJoined(std::string channelName);			// if channel is already in the list, return false, else add + return true 
 		bool removeChannelJoined(std::string channelName);		// if channel is found in the list, erase it + return true, else do nothing and return false
 };
