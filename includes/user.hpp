08e9a9bff66063bbb85fb916c7ce19b79ae0cdb3
twagner <54572786+twagger@users.noreply.github.com>
2022-07-27 14:27:55 +0200

    feat: user mode management with bitwise. addMode / removeMode / hasMode / getMode. Little update on user command to use addMode instead of setMode (#5)

--- includes/user.hpp
+++ includes/user.hpp
@@ -21,6 +21,15 @@
 //      O - local operator flag; 			=> 6	110	
 //      s - marks a user for receipt of server notices. => 7	111
 
+#define MOD_NONE        (0 << 0)
+#define MOD_AWAY        (1 << 0)
+#define MOD_WALLOPS     (1 << 2)
+#define MOD_INVISIBLE   (1 << 3)
+#define MOD_OPER        (1 << 4)
+#define MOD_LOCALOP     (1 << 5)
+#define MOD_SRVNOTICES  (1 << 6)
+#define MOD_RESTRICTED  (1 << 7)
+
 class User {
 
 	private:
@@ -29,7 +38,7 @@ class User {
 		std::string					_username;			
 		std::string 				_fullname;
 		std::string					_hostname;
-		uint16_t					_mode;
+		uint8_t				    	_mode;
 		bool						_password;
 		bool						_authenticated;
 		std::vector<std::string>	_channelsJoined;
@@ -50,7 +59,7 @@ class User {
 		std::string 				getUsername(void) const;
 		std::string 				getFullname(void) const;
 		std::string 				getHostname(void) const;
-		uint16_t 					getMode(void) const;
+		uint8_t 					getMode(void) const;
 		bool 						getPassword(void) const;
 		bool 						getAuthenticated(void) const;
 		std::vector<std::string>	getChannelsJoined(void) const;
@@ -63,13 +72,17 @@ class User {
 		void setUsername(std::string username);
 		void setFullname(std::string fullname);
 		void setHostname(std::string fullname);
-		void setMode(uint16_t mode);
 		void setPassword(bool pass);
 		void setAuthenticated(bool authenticated);
 		void setStatus(int status);
 		void setLastActivityTime(void);
 		void setPingTime(void);
 		
+        // mode
+        void addMode(uint8_t mode);
+        void removeMode(uint8_t mode);
+        bool hasMode(uint8_t mode);
+
 		bool addChannelJoined(std::string channelName);			// if channel is already in the list, return false, else add + return true 
 		bool removeChannelJoined(std::string channelName);		// if channel is found in the list, erase it + return true, else do nothing and return false
 };
