fd5fd82291dfc5371dbec7e9f7052f641137f265
twagner <twagner@student.42.fr>
2022-08-01 23:03:20 +0200

    fix: mask function inversion and addition of escape char for star and question mark

--- srcs/parser/mask.cpp
+++ srcs/parser/mask.cpp
@@ -8,7 +8,7 @@ int match(const char *text, const char *regex);
 static bool matchStar(char c, const char *regex, const char *text)
 {
     do {
-        if (matchMask(regex, text))
+        if (matchMask(text, regex))
             return (true);
     } while (*text != '\0' && (*text++ == c || c == '?'));
     return (false);
@@ -18,9 +18,14 @@ bool    matchMask(const char *text, const char *regex)
 {
     if (regex[0] == '\0')
         return (true);
+	if (regex[0] == '\\' && (regex[1] == '*' || regex[1] == '?'))
+	{
+		if (*text != '\0' && (regex[1] == *text))
+        	return (matchMask(text + 1, regex + 2));
+	}
     if (regex[1] == '*')
         return (matchStar(regex[0], regex + 2, text));
     if (*text != '\0' && (regex[0] == '?' || regex[0] == *text))
-        return (matchMask(regex + 1, text + 1));
+        return (matchMask(text + 1, regex + 1));
     return (false);
 }
