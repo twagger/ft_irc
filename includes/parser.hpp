ef07e2b94a5dc885a1d37a3589f25c3985b242cc
twagner <54572786+twagger@users.noreply.github.com>
2022-08-02 14:54:38 +0200

    Sending messages : PRIVMSG & notice
    * feat: PRIVMSG init with specific exception management to send replies
    
    * feat: parser for mask + few updates
    
    * fix: compilation ok, tests to do on exceptions catch
    
    * save: ok
    
    * feat: all privmsg done and compiling. A lot of tests to do
    
    * feat: utility function for grammar check
    
    * fix: mask function inversion and addition of escape char for star and question mark
    
    * fix: if alignment
    
    * fix: catching exception by value, changed it to ref (in oper.cpp)
    
    * feat: working PRIVMSG + mask matcher
    
    * fix: function to clean doubles in list of targets
    
    * feat: adding notice, privmsg that doesn't send any reply or error back to the sender
    
    * typo on hostname
    
    Co-authored-by: Estelle R <estelle-ad@outlook.com>

--- includes/parser.hpp
+++ includes/parser.hpp
@@ -2,5 +2,6 @@
 # define PARSER_HPP
 
 bool    patternMatch(std::string str, std::string pattern);
+bool    matchMask(const char *text, const char *regex);
 
 #endif
--- srcs/server/Server.cpp
+++ srcs/server/Server.cpp
@@ -103,6 +103,51 @@ User*		Server::getUserByNickname(const std::string &nick) const
     return (NULL);
 }
 
+std::deque<User*>  Server::getUsersByHostname(const std::string &hostname) const
+{
+    std::map<int, User *>::const_iterator   it;
+    std::deque<User*>                       result;
+
+    for (it = this->_userList.begin(); it != this->_userList.end(); ++it)
+	{
+        if (it->second->getHostname() == hostname)
+            result.push_back(it->second);
+    }
+    return (result);
+}
+
+User*		Server::getUserByUsername(const std::string &user, \
+                                const std::string &host) const
+{
+    std::map<int, User *>::const_iterator it;
+
+    for (it = this->_userList.begin(); it != this->_userList.end(); ++it)
+	{
+        if (it->second->getUsername() == user)
+        {
+            if (!host.empty())
+            {
+                if (it->second->getHostname() == host)
+                    return (it->second);
+                else
+                    return (NULL);
+            }
+            return (it->second);
+        }
+    }
+    return (NULL);
+}
+
+std::deque<User*>   Server::getAllUsers(void) const
+{
+    std::map<const int, User *>::const_iterator it;
+    std::deque<User*>                           users;
+
+    for (it = this->_userList.begin(); it != this->_userList.end(); ++it)
+        users.push_back(it->second);
+    return (users);
+}
+
 /* ************************************************************************** */
 /* Private member functions                                                   */
 /* ************************************************************************** */
@@ -261,6 +306,8 @@ void    Server::_initCommandList(void) // functions to complete
     this->_cmdList["VERSION"] = &version;
     this->_cmdList["TIME"] = &time;
     this->_cmdList["INFO"] = &info;
+    this->_cmdList["PRIVMSG"] = &privmsg;
+    this->_cmdList["NOTICE"] = &notice;
     this->_cmdList["CAP"] = &cap;
 }
 
@@ -429,7 +476,7 @@ void    Server::start(void)
 }
 
 // KILL CONNECTION
-void    Server::killConnection(int fd)
+void    Server::killConnection(const int &fd)
 {
     std::map<int, User *>::iterator it;
 
@@ -450,8 +497,11 @@ void    Server::killConnection(int fd)
 }
 
 // SEND CLIENT (ONE FD)
-void    Server::sendClient(int fd, std::string message) const
+void    Server::sendClient(const int &fd, const std::string &message, \
+                           const int &originFd) const
 {
+    if (originFd != -1 && originFd == fd)
+        return;
     if (this->_userList.find(fd) == this->_userList.end())
         throw Server::invalidFdException();
     if (send(fd, message.c_str(), message.length(), MSG_NOSIGNAL) == -1)
@@ -459,16 +509,20 @@ void    Server::sendClient(int fd, std::string message) const
 }
 
 // SEND CLIENT (MULTIPLE FDS)
-void    Server::sendClient(std::set<int> &fds, std::string message) const
+void    Server::sendClient(const std::set<int> &fds, \
+                           const std::string &message, \
+                           const int &originFd) const
 {
-    std::set<int>::iterator it;
+    std::set<int>::const_iterator it;
 
     for (it = fds.begin(); it != fds.end(); ++it)
-        this->sendClient(*it, message);
+        this->sendClient(*it, message, originFd);
 }
 
 // SEND CHANNEL
-void    Server::sendChannel(std::string channel, std::string message) const
+void    Server::sendChannel(const std::string &channel, \
+                            const std::string &message, \
+                            const int &originFd) const
 {
     std::map<std::string, Channel *>::const_iterator    itChannel;
     std::deque<User *>::const_iterator                  itUsers;
@@ -480,13 +534,13 @@ void    Server::sendChannel(std::string channel, std::string message) const
     userList = itChannel->second->getUsers();
     for (itUsers = userList.begin(); itUsers != userList.end(); ++itUsers) {
 		if (((*itUsers)->getFd() != 0)) {
-        	this->sendClient((*itUsers)->getFd(), message);
+        	this->sendClient((*itUsers)->getFd(), message, originFd);
 		}
 	}
 }
 
 // BROADCAST
-void    Server::broadcast(std::string message) const
+void    Server::broadcast(const std::string &message,const int &originFd) const
 {
     std::map<const int, User *>::const_iterator it;
     std::set<int>                               fds;
@@ -494,7 +548,7 @@ void    Server::broadcast(std::string message) const
     for (it = this->_userList.begin(); it != this->_userList.end(); ++it)
     {
         fds.insert(it->first);
-        this->sendClient(fds, message);
+        this->sendClient(fds, message, originFd);
     }
 }
 
