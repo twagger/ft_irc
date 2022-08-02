8b20c76458134bde61de5935d0366d6a04cc40aa
twagner <54572786+twagger@users.noreply.github.com>
2022-08-02 16:18:24 +0200

    Miscellaneous messages : review of PING, PONG, KILL and minor fixes (#19)
    * feat: PRIVMSG init with specific exception management to send replies
    
    * feat: parser for mask + few updates
    
    * fix: compilation ok, tests to do on exceptions catch
    
    * save: ok
    
    * feat: all privmsg done and compiling. A lot of tests to do
    
    * feat: utility function for grammar check
    
    * fix: mask function inversion and addition of escape char for star and question mark
    
    * fix: if alignment
    
    * fix: catching exception by value, changed it to ref (in oper.cpp)
    
    * feat: working PRIVMSG + mask matcher
    
    * fix: function to clean doubles in list of targets
    
    * feat: adding notice, privmsg that doesn't send any reply or error back to the sender
    
    * fix: server, ping client and unused include on time.cpp

--- srcs/commands/kill.cpp
+++ srcs/commands/kill.cpp
@@ -1,5 +1,6 @@
 #include "../../includes/commands.hpp"
 #include "../../includes/utils.hpp"
+#include "../../includes/exceptions.hpp"
 
 void    kill(const int &fd, const std::vector<std::string> &params, \
                        const std::string &,Server *srv)
@@ -8,43 +9,40 @@ void    kill(const int &fd, const std::vector<std::string> &params, \
     std::string comment;
     User        *target;
 
-    // param check ----------------------------------------------------------- /
-    // check nb of param
-    if (params.size() != 2)
-    {
-        srv->sendClient(fd, \
-		numericReply(srv, fd, "461", ERR_NEEDMOREPARAMS(std::string("KILL")))); 
-        return;
+    // COMMAND EXECUTION
+    try {
+        // check params
+        if (params.size() < 2)
+            throw needmoreparamsException("KILL");
+
+        nickname = params[0];
+        comment = params[1];
+        // check if nickname is actually the server hostname
+        // This involves that you cannot have a nickname = to server hostname
+        if (nickname.compare(srv->getHostname()) == 0)
+            throw cantkillserverException();
+
+        // check if nickname exists
+        target = srv->getUserByNickname(nickname);
+        if (target == NULL)
+            throw nosuchnickException(nickname);
+
+        // check if user associated with the connection is Op
+        if (!srv->getUserByFd(fd)->hasMode(MOD_OPER))
+            throw noprivilegesException();
+
+        // all is ok, execute the KILL
+        try { srv->killConnection(target->getFd()); }
+        catch (Server::pollDelException &e) { printError(e.what(), 1, true); }
+        catch (Server::invalidFdException &e) { printError(e.what(), 1, false); }
+
+        // add the nickname to the list of unavailable nicknames with a timer
+        srv->_unavailableNicknames[nickname] = time(NULL);
     }
-    nickname = params[0];
-    comment = params[1];
-    // check if nickname exists
-    target = srv->getUserByNickname(nickname);
-    if (target == NULL)
-    {
-        srv->sendClient(fd, \
-            numericReply(srv, fd, "401", ERR_NOSUCHNICK(nickname)));
-        return;
-    }
 
-
-	// TO DO: add ERR_CANTKILLSERVER case
-
-
-    // check if user associated with the connection is Op
-    if (!srv->getUserByFd(fd)->hasMode(MOD_OPER))
-    {
-		std::cout << target->hasMode(MOD_OPER) << std::endl;
-        srv->sendClient(fd, \
-            numericReply(srv, fd, "481", ERR_NOPRIVILEGES(nickname)));
-        return;
-    }
-
-    // all is ok, execute the KILL ------------------------------------------- /
-    try { srv->killConnection(target->getFd()); }
-    catch (Server::pollDelException &e) { printError(e.what(), 1, true); }
-    catch (Server::invalidFdException &e) { printError(e.what(), 1, false); }
-
-    // add the nickname to the list of unavailable nicknames with a timer
-    srv->_unavailableNicknames[nickname] = time(NULL);
+    // EXCEPTIONS
+    catch (noprivilegesException &e) {e.reply(srv, fd); return; }
+    catch (nosuchnickException &e) {e.reply(srv, fd); return; }
+    catch (needmoreparamsException &e) {e.reply(srv, fd); return; }
+    catch (cantkillserverException &e) {e.reply(srv, fd); return; }
 }
--- srcs/commands/pong.cpp
+++ srcs/commands/pong.cpp
@@ -1,22 +1,36 @@
 #include "../../includes/commands.hpp"
 #include "../../includes/utils.hpp"
+#include "../../includes/exceptions.hpp"
 
 void pong(const int &fd, const std::vector<std::string> &params, \
                        const std::string &, Server *srv)
 {
-    std::string clientname;
+    std::string origin;
+    std::string servername;
     User        *user;
 
-    // check nb of param
-    if (params.size() == 1) // only clientname is expected
-    {
-        clientname = params.front();
-        // update client status
-        if ((user = srv->getUserByFd(fd)) == NULL)
+    // COMMAND EXECUTION
+    try {
+        if (params.size() == 0)
+            throw nooriginException();
+        if (params.size() >= 1)
         {
-            printError("Unknown user", 1, false);
-            return;
+            origin = params[0];
+            if (params.size() == 2)
+            {
+                // In mono-server, the target server must be the current one
+                servername = params[1];
+                if (srv->getHostname().compare(servername) != 0)
+                    throw nosuchserverException(servername);
+            }
+
+            // update client status with the fd, as we are not in multiserv
+            if ((user = srv->getUserByFd(fd)) != NULL)
+                user->setStatus(ST_ALIVE);           
         }
-        user->setStatus(ST_ALIVE);
     }
+
+    // EXCEPTIONS
+    catch (nooriginException &e) {e.reply(srv, fd); return; }
+    catch (nosuchserverException &e) {e.reply(srv, fd); return; }
 }
