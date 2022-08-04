6f9c8042f7298773105da75bd198013450869752
twagner <twagner@student.42.fr>
2022-08-04 11:43:29 +0200

    feat: who ok

--- srcs/commands/who.cpp
+++ srcs/commands/who.cpp
@@ -19,17 +19,18 @@ std::string getCommonChannel(Server *srv, const int &fd1, const int &fd2)
 {
     std::deque<std::string>                 channelsJoined1;
     std::deque<std::string>                 channelsJoined2;
-    std::deque<std::string>::const_iterator it;
-    std::deque<std::string>::const_iterator itRes;
+    std::deque<std::string>::const_reverse_iterator it;
+    std::deque<std::string>::const_reverse_iterator itRes;
     User                                    *user1 = srv->getUserByFd(fd1);
     User                                    *user2 = srv->getUserByFd(fd2);
 
     channelsJoined1 = user1->getChannelsJoined();
-    for (it = channelsJoined1.begin(); it != channelsJoined1.end(); ++it)
+    for (it = channelsJoined1.rbegin(); it != channelsJoined1.rend(); ++it)
     {
         channelsJoined2 = user2->getChannelsJoined();
-        itRes = std::find(channelsJoined2.begin(), channelsJoined2.end(), *it);
-        if (itRes != user2->getChannelsJoined().end())
+        itRes = std::find(channelsJoined2.rbegin(), channelsJoined2.rend(), \
+                          *it);
+        if (itRes != channelsJoined2.rend())
             return (*it);
     }
     return (std::string());
@@ -38,12 +39,24 @@ std::string getCommonChannel(Server *srv, const int &fd1, const int &fd2)
 bool    isOnTheSameChannel(Server *srv, const int &fd1, const int &fd2)
 { return (!getCommonChannel(srv, fd1, fd2).empty()); }
 
+bool    isUserChanOper(Server *srv, User *user, const std::string channel)
+{
+    std::deque<User *>                  userList;
+    std::deque<User *>::const_iterator  it;
+
+    userList = srv->_channelList[channel]->_operators;
+    if (std::find(userList.begin(), userList.end(), user) == userList.end())
+        return (false);
+    return (true);
+}
+
 // Send information about users visible for me
 void who(const int &fd, const std::vector<std::string> &params, \
          const std::string &, Server *srv)
 {
     std::string                 mask;
     std::string                 name;
+    std::string                 channel;
     bool                        onlyOpers = false;
     std::deque<User*>           usersList;
     std::deque<User*>           allUsers;
@@ -72,60 +85,67 @@ void who(const int &fd, const std::vector<std::string> &params, \
 
     if (srv->_channelList.find(mask) != srv->_channelList.end()) {
         // 2. Is the match a channel ? Erase all users not in that channel
-        for (it = usersList.begin(); it != usersList.end(); it++)
+        for (it = usersList.begin(); it != usersList.end();)
         {
             channelJoined = (*it)->getChannelsJoined();
             if (std::find(channelJoined.begin(), channelJoined.end(), mask) \
                     == channelJoined.end())
             {
-                usersList.erase(it);
+                it = usersList.erase(it);
             }
+            else
+                ++it;
         }
     }
     else {
         // 3. The mask is not a channel : Check the mask against users's Host, 
         // Server, Real name, Nickname
-        for (it = usersList.begin(); it != usersList.end(); it++)
+        for (it = usersList.begin(); it != usersList.end();)
         {
             // Host
-            if (matchMask((*it)->getHostname().c_str(), mask.c_str()) == true)
-                continue;
+            if ((matchMask((*it)->getHostname().c_str(), mask.c_str()) == true)
             // Server
-            if (matchMask(srv->getHostname().c_str(), mask.c_str()) == true)
-                continue;
+            || (matchMask(srv->getHostname().c_str(), mask.c_str()) == true)
             // Real name
-            if (matchMask((*it)->getFullname().c_str(), mask.c_str()) == true)
-                continue;
+            || (matchMask((*it)->getFullname().c_str(), mask.c_str()) == true)
             // Nickname
-            if (matchMask((*it)->getNickname().c_str(), mask.c_str()) == true)
+            || (matchMask((*it)->getNickname().c_str(), mask.c_str()) == true))
+            {
+                ++it;
                 continue;
-            usersList.erase(it);
+            }
+            else
+                it = usersList.erase(it);
         }
     }
 
     if (onlyOpers == true) {
         // 4. If "o" option is set, apply oper option on the list
-        for (it = usersList.begin(); it != usersList.end(); it++)
+        for (it = usersList.begin(); it != usersList.end();)
         {
             // If user hasMode(MOD_OPER) == false > Remove
             if ((*it)->hasMode(MOD_OPER) == false)
-                usersList.erase(it);
+                it = usersList.erase(it);
+            else
+                ++it;
         }
     }
 
     // 5. Loop on the resulting user list and send information about users
     for (it = usersList.begin(); it != usersList.end(); it++)
     {
+        channel = getCommonChannel(srv, fd, (*it)->getFd());
         srv->sendClient(fd, \
            numericReply(srv, fd, "352", RPL_WHOREPLY(\
-            getCommonChannel(srv, fd, (*it)->getFd()), \
+            channel, \
             (*it)->getUsername(), \
             (*it)->getHostname(), \
             srv->getHostname(), \
             (*it)->getNickname(), \
             std::string("H"), \
             ( (*it)->hasMode(MOD_OPER) ? std::string("*") : std::string() ), \
-            std::string(), \
+            ( isUserChanOper(srv, *it, channel) ? std::string("@") 
+                                                : std::string() ), \
             (*it)->getFullname())));
     }
     if (params.size() > 0)
