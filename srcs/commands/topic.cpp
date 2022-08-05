704a47ea1db4bca8b91c2e2edf7833f39474a827
Lea LESCURE <llescure@e2r3p6.clusters.42paris.fr>
2022-08-05 12:32:18 +0200

    fix(topic): topic doesn't need two points before the name

--- srcs/commands/topic.cpp
+++ srcs/commands/topic.cpp
@@ -13,17 +13,15 @@
  *   
  */
 
-int checkTopicParameter(std::string topic, std::map<std::string, Channel *>::iterator itChannel,
+int checkTopicParameter(std::map<std::string, Channel *>::iterator itChannel,
     Server *server, const int &fdUser)
 {
-    // Topic format is not valid
-    if (topic.find(':') != 0)
-        return (-1);
-    // Current user is not in user list
     User *currentUser = server->getUserByFd(fdUser);
+
     if (findUserOnChannel(itChannel->second->_users, currentUser) == false)
     {
-        server->sendClient(fdUser, numericReply(server, fdUser, "442", ERR_NOTONCHANNEL(itChannel->first)));
+        server->sendClient(fdUser, numericReply(server, fdUser,
+            "442", ERR_NOTONCHANNEL(itChannel->first)));
         return (-2);
     }
     return (0);
@@ -51,10 +49,11 @@ int checkGeneralParameter(const int &fdUser, const std::vector<std::string> &par
 void changeTopic(std::string topic, std::map<std::string, Channel *>::iterator itChannel,
     Server *server, const int &fdUser)
 {
-    if (checkTopicParameter(topic, itChannel, server, fdUser) < 0)
+    if (checkTopicParameter(itChannel, server, fdUser) < 0)
         return ;
     itChannel->second->setTopic(topic);
-    return (server->sendChannel(itChannel->first, clientReply(server, fdUser, "TOPIC " + itChannel->first + " " + topic)));
+    return (server->sendChannel(itChannel->first, clientReply(server, fdUser,
+        "TOPIC " + itChannel->first + " " + topic)));
 }
 
 void topic(const int &fdUser, const std::vector<std::string> &parameter,
@@ -73,7 +72,9 @@ void topic(const int &fdUser, const std::vector<std::string> &parameter,
     if (parameter.size() > 1)
         changeTopic(parameter[1], itChannel, server, fdUser);
     else if (itChannel->second->getTopic().empty() == false)
-        server->sendClient(fdUser, numericReply(server, fdUser, "332", RPL_TOPIC(channel, itChannel->second->getTopic())));
+        server->sendClient(fdUser, numericReply(server, fdUser,
+            "332", RPL_TOPIC(channel, itChannel->second->getTopic())));
     else
-        server->sendClient(fdUser, numericReply(server, fdUser, "331", RPL_NOTOPIC(channel)));
+        server->sendClient(fdUser, numericReply(server, fdUser,
+            "331", RPL_NOTOPIC(channel)));
 }
